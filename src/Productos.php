68f2a000948280f72da9cbb9b0350bc3cb139f1e
Juan Iriart <jeiriart@gmail.com>
2022-02-24 17:25:45 -0300

    addede npm

---
+++ src/Productos.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mate\Int;
+
+class Products {
+    public static function show_products() {
+        return [
+            [
+                'name' => 'Producto 1',
+                'price' => 1000
+            ],
+            [
+                'name' => 'Producto 2',
+                'price' => 2000
+            ]
+        ];
+    }
+}
